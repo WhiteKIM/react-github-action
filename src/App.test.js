529345377903febad43d35bed2896d918a5ab688
WhiteKIM <9rudrb40@naver.com>
2023-06-26 14:40:15 +0900

    2023.06.26 init

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,55 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import App from './App';
 
-test('renders learn react link', () => {
+test('the Counter Starts at 0', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const counterElement = screen.getByTestId('counter');
+  expect(counterElement).toHaveTextContent(0);
 });
+
+test('minus button has correct text', () =>{
+  render(<App />);
+
+  const buttonElement = screen.getByTestId("minus-button");
+  expect(buttonElement).toHaveTextContent("-");
+})
+
+test('plus button has correct text', () =>{
+  render(<App />);
+
+  const buttonElement = screen.getByTestId("plus-button");
+  expect(buttonElement).toHaveTextContent("+");
+})
+
+test('When the + button is pressed', () =>{
+  render(<App />)
+  const buttonElement = screen.getByTestId("plus-button");
+  fireEvent.click(buttonElement);
+  const counterElement = screen.getByTestId('counter');
+  expect(counterElement).toHaveTextContent(1);
+})
+
+test('When the - button is pressed', () =>{
+  render(<App />)
+  const buttonElement = screen.getByTestId("minus-button");
+  fireEvent.click(buttonElement);
+  const counterElement = screen.getByTestId('counter');
+  expect(counterElement).toHaveTextContent(-1);
+})
+
+test('on/off button has blue color',()=>{
+  render(<App />)
+  const buttonElement = screen.getByTestId("on/off-button");
+  expect(buttonElement).toHaveStyle({backgroundColor : 'blue'});
+})
+
+test("Prevent the -, + button from being pressed of/off button", () =>{
+  render(<App />)
+  const onOffButtonElement = screen.getByTestId("on/off-button");
+  fireEvent.click(onOffButtonElement);
+  const plusButtonElement = screen.getByTestId("plus-button");
+  expect(plusButtonElement).toBeDisabled();
+  const minusButtonElement = screen.getByTestId("minus-button");
+  expect(minusButtonElement).toBeDisabled();
+})
